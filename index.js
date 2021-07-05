9e6fc6031dbc4b2182bfc6e6f47957e9249b34c0
Anirup Patnaik <anirup049@gmail.com>
2021-07-06 03:03:09 +0530

    Local storage explained

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hi");
+localStorage.setItem("name", "Anirup");
+localStorage.setItem("cartItemCount", 10);
+
+let cartItems = [
+  {
+    name: "Macbook",
+    quantity: 1,
+  },
+  {
+    name: "iPhone",
+    quantity: 1,
+  },
+];
+
+localStorage.setItem("cartItem", JSON.stringify(cartItems));
+console.log(JSON.parse(localStorage.getItem("cartItem")));
